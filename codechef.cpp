6a74226df6490e99dba97191e0e90bddcf5c8458
acevaliant <202211070@diu.iiitvadodara.ac.in>
2024-11-07 11:43:33 +0530

    Codechef Contest

---
+++ codechef.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Coded by Rajat Kumar Thakur (ace_valiant69)
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+#define MOD 1000000007
+
+const int tab64[64] = {
+    63,  0, 58,  1, 59, 47, 53,  2,
+    60, 39, 48, 27, 54, 33, 42,  3,
+    61, 51, 37, 40, 49, 18, 28, 20,
+    55, 30, 34, 11, 43, 14, 22,  4,
+    62, 57, 46, 52, 38, 26, 32, 41,
+    50, 36, 17, 19, 29, 10, 13, 21,
+    56, 45, 25, 31, 35, 16,  9, 12,
+    44, 24, 15,  8, 23,  7,  6,  5};
+
+int log2_64 (uint64_t value)
+{
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    value |= value >> 32;
+    return tab64[((uint64_t)((value - (value >> 1))*0x07EDD5E59A4E28C2)) >> 58];
+}
+ll int largestdiv(ll int n){
+    for(ll int i=2; i<=sqrt(n); i++){
+        if(n%i==0){
+            return i;
+        }
+    }
+    return 1;
+}
+
+ll int powerOfTwo(ll int base, ll int exponent) {
+    return base << exponent;
+}
+
+void solve(){
+    ll int h, k;
+    cin>>h>>k;
+    if(h==1&&k==1){
+        cout<<0<<endl;
+    }
+    ll int count1 = 1;
+    for(ll int m=1; m<=k; m = m*2){
+        ll int temp = m*h;
+        ll int count = 1;
+        while(largestdiv(temp)!=1){
+            temp = temp/largestdiv(temp);
+            count++;
+        }
+        count1 = max(count1, count);
+    }
+    
+    cout<<count1<<endl;
+}
+
+
+int main()
+{
+
+	int tc;
+	cin>>tc;
+
+	while(tc--){
+		solve();
+	}
+	return 0;
+}
